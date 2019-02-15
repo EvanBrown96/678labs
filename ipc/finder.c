8885710d704382cf48e9e8793ab4547d01c8d22d
EvanBrown96 <evan.br96@yahoo.com>
2019-02-15 14:44:17 -0600

    testing

--- ipc/finder.c
+++ ipc/finder.c
@@ -24,7 +24,12 @@ int main(int argc, char *argv[])
     exit(0);
   }
 
+  int p1[2], p2[2], p3[2];
+
   // create pipes
+  pipe(p1);
+  pipe(p2);
+  pipe(p3);
 
   pid_1 = fork();
   if (pid_1 == 0) {
@@ -36,43 +41,45 @@ int main(int argc, char *argv[])
     close(p3[0]);
     close(p3[1]);
 
-    char* myArgs[] = {BASH_EXEC, GREP_EXEC, "-c", cmdbuf, (char*) NULL};
+    //cmdbuf?
+
+    char* myArgs[] = {BASH_EXEC, FIND_EXEC, "-c", "execute", (char*) NULL};
     if(execv(BASH_EXEC, myArgs) < 0){
       fprintf(stderr, "\nerror #%d\n", errno);
       return EXIT_FAILURE;
     }
     exit(0);
   }
-
-  pid_2 = fork();
-  if (pid_2 == 0) {
-    /* Second Child */
-    dup2(p1[0], STDIN_FILENO);
-    close(p1[1]);
-    close(p2[0]);
-    dup2(p2[1], STDOUT_FILENO);
-    close(p3[0]);
-    close(p3[1]);
-
-    char* myArgs[] = {XARGS_EXEC, GREP_EXEC, "-c", argv[2], (char*) NULL};
-    if(execv(XARGS_EXEC, myArgs) < 0){
-      fprintf(stderr, "\nerror #%d\n", errno);
-      return EXIT_FAILURE;
-    }
-    exit(0);
-  }
-
-  pid_3 = fork();
-  if (pid_3 == 0) {
-    /* Third Child */
-    exit(0);
-  }
-
-  pid_4 = fork();
-  if (pid_4 == 0) {
-    /* Fourth Child */
-    exit(0);
-  }
+  //
+  // pid_2 = fork();
+  // if (pid_2 == 0) {
+  //   /* Second Child */
+  //   dup2(p1[0], STDIN_FILENO);
+  //   close(p1[1]);
+  //   close(p2[0]);
+  //   dup2(p2[1], STDOUT_FILENO);
+  //   close(p3[0]);
+  //   close(p3[1]);
+  //
+  //   char* myArgs[] = {XARGS_EXEC, GREP_EXEC, "-c", argv[2], (char*) NULL};
+  //   if(execv(XARGS_EXEC, myArgs) < 0){
+  //     fprintf(stderr, "\nerror #%d\n", errno);
+  //     return EXIT_FAILURE;
+  //   }
+  //   exit(0);
+  // }
+  //
+  // pid_3 = fork();
+  // if (pid_3 == 0) {
+  //   /* Third Child */
+  //   exit(0);
+  // }
+  //
+  // pid_4 = fork();
+  // if (pid_4 == 0) {
+  //   /* Fourth Child */
+  //   exit(0);
+  // }
 
   close(p1[0]);
   close(p1[1]);
@@ -85,18 +92,18 @@ int main(int argc, char *argv[])
     fprintf(stderr, "Process 1 encountered an error. ERROR%d", errno);
     return EXIT_FAILURE;
   }
-  if ((waitpid(pid_2, &status, 0)) == -1) {
-    fprintf(stderr, "Process 2 encountered an error. ERROR%d", errno);
-    return EXIT_FAILURE;
-  }
-  if ((waitpid(pid_3, &status, 0)) == -1) {
-    fprintf(stderr, "Process 3 encountered an error. ERROR%d", errno);
-    return EXIT_FAILURE;
-  }
-  if ((waitpid(pid_4, &status, 0)) == -1) {
-    fprintf(stderr, "Process 4 encountered an error. ERROR%d", errno);
-    return EXIT_FAILURE;
-  }
+  // if ((waitpid(pid_2, &status, 0)) == -1) {
+  //   fprintf(stderr, "Process 2 encountered an error. ERROR%d", errno);
+  //   return EXIT_FAILURE;
+  // }
+  // if ((waitpid(pid_3, &status, 0)) == -1) {
+  //   fprintf(stderr, "Process 3 encountered an error. ERROR%d", errno);
+  //   return EXIT_FAILURE;
+  // }
+  // if ((waitpid(pid_4, &status, 0)) == -1) {
+  //   fprintf(stderr, "Process 4 encountered an error. ERROR%d", errno);
+  //   return EXIT_FAILURE;
+  // }
 
   return 0;
 }
