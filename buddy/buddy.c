7c862914b6fb5dc964948ed3db7c566a37d3d8fa
EvanBrown96 <evanbrown@ku.edu>
2019-04-26 17:23:26 -0500

    added more comments

--- buddy/buddy.c
+++ buddy/buddy.c
@@ -71,6 +71,15 @@ page_t g_pages[(1<<MAX_ORDER)/PAGE_SIZE];
  * Public Function Prototypes
  **************************************************************************/
 
+/**
+ * splits the first free page of the given order into two smaller pages
+ *
+ * assumptions made:
+ * - the first page of the free list of the given order has been properly marked as free
+ * - there are no free blocks of size order-1 (otherwise why would we need to split?)
+ *
+ * @param order size of block to split up
+ */
 void buddy_split(int order){
 
 	// get first free page of the desired size
@@ -87,11 +96,25 @@ void buddy_split(int order){
 	buddy_page->block_size = order-1;
 
 	// add both to free list of one less order
+	// (free_page will be first, buddy_page second)
 	list_add(&(buddy_page->list), &free_area[order-1]);
 	list_add(&(free_page->list), &free_area[order-1]);
 
 }
 
+/**
+ * combines two blocks
+ *
+ * order of blocks does not matter
+ *
+ * assumptions made:
+ * - buddy_page points to the correct buddy of the_page based on the_page's block_size
+ * - the_page is properly marked as free
+ *
+ * @param the_page pointer to the page at the start of a block
+ * @param buddy_page pointer to the page at the start of that block's buddy
+ * @return pointer to the page resulting from the attempted combination
+ */
 page_t* buddy_combine(page_t* the_page, page_t* buddy_page){
 
 	// give back original page if its buddy is not free
@@ -134,7 +157,6 @@ void buddy_init()
 	int i;
 	int n_pages = (1<<MAX_ORDER) / PAGE_SIZE;
 	for (i = 0; i < n_pages; i++) {
-		//INIT_LIST_HEAD(&(g_pages[i].list));
 		g_pages[i].index = i;
 		g_pages[i].is_free = 1;
 		g_pages[i].block_size = 0;
@@ -145,6 +167,7 @@ void buddy_init()
 		INIT_LIST_HEAD(&free_area[i]);
 	}
 
+	// let the first page know it is the start of a block with size of the entire memory
 	g_pages[0].block_size = MAX_ORDER;
 	/* add the entire memory as a freeblock */
 	list_add(&g_pages[0].list, &free_area[MAX_ORDER]);
@@ -213,13 +236,21 @@ void buddy_free(void *addr)
 	page_t* page_to_free = &(g_pages[ADDR_TO_PAGE(addr)]);
 	page_to_free->is_free = 1;
 
+	// get the page at the start of the block corresponding to the buddy of the initial block
 	page_t* buddy_page = &(g_pages[ADDR_TO_PAGE(BUDDY_ADDR(addr, page_to_free->block_size))]);
 
+	// attempt to combine the page with its buddy
 	page_t* res_page = buddy_combine(page_to_free, buddy_page);
 
+	// get head of free list the block should be inserted into
 	struct list_head* free_head = &free_area[res_page->block_size];
+	// start iterator at the head of that list
 	void* insert_loc = free_head;
 	page_t* test_page;
+	// iterate through the free list for this block size
+	// once we find a block in the list with a greater index than the block we're trying to insert,
+	// save the list_head associated with that entry to insert our block before.
+	// if no blocks are found with a greater index, our block is inserted at the end of the list
 	list_for_each_entry(test_page, free_head, list){
 		if(res_page->index < test_page->index){
 			insert_loc = &(test_page->list);
@@ -227,6 +258,7 @@ void buddy_free(void *addr)
 		}
 	}
 
+	// add block at whatever location we found
 	list_add_tail(&(res_page->list), insert_loc);
 
 }
